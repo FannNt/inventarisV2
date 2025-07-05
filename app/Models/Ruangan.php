cc778411a187ec455ff3275d0bf2e606390f5733
Fannn <affanhelmi111@gmail.com>
2025-07-05 11:41:29 +0700

    remove auto increment

--- app/Models/Ruangan.php
+++ app/Models/Ruangan.php
@@ -11,6 +11,32 @@ class Ruangan extends Model
 
     protected $guarded = [];
 
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $prefix = 'RI';
+
+                $lastId = static::where('id', 'like', $prefix . '%')
+                    ->orderByDesc('id')
+                    ->first()?->id;
+
+                $nextNumber = 1;
+
+                if ($lastId) {
+                    $numberPart = (int) substr($lastId, strlen($prefix));
+                    $nextNumber = $numberPart + 1;
+                }
+
+                $model->id = $prefix . str_pad($nextNumber, 3, '0', STR_PAD_LEFT);
+            }
+        });
+    }
     public function item()
     {
         return $this->hasMany(Item::class,'ruangan_id');
--- database/migrations/2025_06_23_061517_create_ruangans_table.php
+++ database/migrations/2025_06_23_061517_create_ruangans_table.php
@@ -12,7 +12,7 @@ return new  class extends Migration
     public function up(): void
     {
         Schema::create('ruangans', function (Blueprint $table) {
-            $table->id();
+            $table->string('id',5)->primary();
             $table->string('name');
             $table->timestamps();
         });
