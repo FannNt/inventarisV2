83a38d85afe1a6caee0254b90400b427543a842b
Fannn <affanhelmi111@gmail.com>
2025-07-20 14:08:32 +0700

    rollback migration modification and make new migration to remove that field

--- database/migrations/2025_07_05_055559_create_item_statuses_table.php
+++ database/migrations/2025_07_05_055559_create_item_statuses_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('item_statuses', function (Blueprint $table) {
             $table->id();
+            $table->string('item_id');
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade')->onUpdate('cascade');
             $table->string('condition');
             $table->string('keterangan')->nullable();
             $table->timestamps();
